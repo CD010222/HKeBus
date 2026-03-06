9547367f5430cae17bcb494f789bb62648e9acd5
CD010222 <1054079400@qq.com>
2026-03-06 20:42:01 +0800

    chinaEbus initial

--- docs/map/china-city.md
+++ docs/map/china-city.md
@@ -1,6 +1,7 @@
 ---
 layout: page
 ---
+# ChinaEBus
 
 <ChinaCityMap />
 
--- docs/src/components/ChinaCityMap.vue
+++ docs/src/components/ChinaCityMap.vue
@@ -1,11 +1,20 @@
 <template>
-  <MapComponent
-    :center="[105, 35]"
-    :zoom="3.5"
-    width="100%"
-    height="80vh"
-    @map-loaded="handleMapLoaded"
-  />
+  <div>
+    <MapComponent
+      :center="[105, 35]"
+      :zoom="3.5"
+      width="100%"
+      height="80vh"
+      @map-loaded="handleMapLoaded"
+    />
+    <div 
+      v-if="tooltip.show" 
+      class="tooltip"
+      :style="{ left: tooltip.x + 'px', top: tooltip.y + 'px' }"
+    >
+      {{ tooltip.name2 }}
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -18,58 +27,17 @@ const center = [105, 35];
 const tooltip = ref({ show: false, x: 0, y: 0, name2: '', nlName: '' });
 
 let deckMap = null;
+let geojsonData = null;
 
 onMounted(async () => {
-  // 预加载 GeoJSON 数据
   try {
     const response = await fetch('/HKebus/china_city.geojson');
     const data = await response.json();
+    geojsonData = data;
     
-    deckMap.setProps({
-      layers: [
-        new GeoJsonLayer({
-          id: 'china-cities',
-          data: data,
-          filled: true,
-          stroked: true,
-          lineWidthMinPixels: 1,
-          getFillColor: [0, 100, 200, 150],
-          getLineColor: [255, 255, 255, 100],
-          getLineWidth: 1,
-          pickable: true,
-          autoHighlight: true,
-          highlightColor: [255, 200, 0, 200],
-          
-          onHover: info => {
-            if (info.object) {
-              const name2 = info.object.properties.NAME_2 || '未知';
-              const nlName = info.object.properties.NL_NAME_2 || '';
-              tooltip.value = {
-                show: true,
-                x: info.x,
-                y: info.y,
-                name2: name2,
-                nlName: nlName
-              };
-              document.body.style.cursor = 'pointer';
-            } else {
-              tooltip.value.show = false;
-              document.body.style.cursor = 'default';
-            }
-          },
-          
-          onClick: info => {
-            if (info.object) {
-              const name2 = info.object.properties.NAME_2;
-              if (name2) {
-                const url = `/HKebus/map/city/${encodeURIComponent(name2)}.html`;
-                window.open(url, '_blank');
-              }
-            }
-          }
-        })
-      ]
-    });
+    if (deckMap) {
+      updateDeckMap();
+    }
   } catch (error) {
     console.error('Error loading GeoJSON:', error);
   }
@@ -77,6 +45,57 @@ onMounted(async () => {
 
 const handleMapLoaded = (map) => {
   deckMap = useDeckOverlay(map);
+  if (geojsonData) {
+    updateDeckMap();
+  }
+};
+
+const updateDeckMap = () => {
+  deckMap.setProps({
+    layers: [
+      new GeoJsonLayer({
+        id: 'china-cities',
+        data: geojsonData,
+        filled: true,
+        stroked: true,
+        lineWidthMinPixels: 1,
+        getFillColor: [0, 100, 200, 150],
+        getLineColor: [255, 255, 255, 100],
+        getLineWidth: 1,
+        pickable: true,
+        autoHighlight: true,
+        highlightColor: [255, 200, 0, 200],
+        
+        onHover: info => {
+          if (info.object) {
+            const name2 = info.object.properties.NAME_2 || '未知';
+            const nlName = info.object.properties.NL_NAME_2 || '';
+            tooltip.value = {
+              show: true,
+              x: info.x,
+              y: info.y,
+              name2: name2,
+              nlName: nlName
+            };
+            document.body.style.cursor = 'pointer';
+          } else {
+            tooltip.value.show = false;
+            document.body.style.cursor = 'default';
+          }
+        },
+        
+        onClick: info => {
+          if (info.object) {
+            const name2 = info.object.properties.NAME_2;
+            if (name2) {
+              const url = `/HKebus/map/city/${encodeURIComponent(name2)}.html`;
+              window.open(url, '_blank');
+            }
+          }
+        }
+      })
+    ]
+  });
 };
 
 onUnmounted(() => {
